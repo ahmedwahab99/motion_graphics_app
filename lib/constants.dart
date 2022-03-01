c7c7752f82bedb4cd3890fe2a7ba2e20fd41dcf3
ahmedwahab99 <aadabdalwahab99.aa@gmail.com>
2022-03-01 19:27:34 +0200

    news card

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,3 +5,8 @@ class ConstColors{
   static Color purple = const Color(0xffB7266F);
   static Color blue = const Color(0xff327FBB);
 }
+
+class Labels{
+  static String appName = 'Aymoontage';
+  
+}
--- lib/view/screens/signin_up.dart
+++ lib/view/screens/signin_up.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:motion_graphics_app/constants.dart';
 
 class Signin extends StatefulWidget {
-  const Signin({ Key? key }) : super(key: key);
+  const Signin({Key? key}) : super(key: key);
 
   @override
   _SigninState createState() => _SigninState();
@@ -11,51 +13,95 @@ class _SigninState extends State<Signin> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3, /// same count as tapBar items
+      length: 2,
+
+      /// same count as tapBar items
       child: Scaffold(
-        appBar:AppBar(
-              title: const Text("title"),
-              //centerTitle: true,
-              leading: IconButton(
-                icon: const Icon(Icons.menu),
-                onPressed: () {},
-              ),
-              actions: [
-                IconButton(
-                  icon: const Icon(Icons.notifications_none),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: const Icon(Icons.search),
-                  onPressed: () {},
-                )
-              ],
-              //backgroundColor: Colors.purple,
-              flexibleSpace: Container(
-                
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [Colors.purple, Colors.red],
-                    begin: Alignment.bottomRight,
-                    end: Alignment.topLeft,
-                  ),
-                ),
-              ),
-              bottom: const TabBar(
-                //isScrollable: true,
-                indicatorColor: Colors.white,
-                indicatorWeight: 5,
-                tabs: [
-                  //Tab(icon: const Icon(Icons.home), text: 'Home'),
-                  const Tab(icon: const Icon(Icons.star), text: 'Feed'),
-                  Tab(icon: Icon(Icons.face), text: 'Profile'),
-                  Tab(icon: Icon(Icons.settings), text: 'Settings'),
-                ],
-              ),
-              elevation: 20,
-              titleSpacing: 20,
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(250),
+          child: AppBar(
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () {},
             ),
-        
+            flexibleSpace: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [ConstColors.purple, ConstColors.blue],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                ),
+              ),
+              child: Center(
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 100,
+                    ),
+                    Text(
+                      Labels.appName,
+                      style: GoogleFonts.dancingScript(
+                          fontSize: 40, color: Colors.white),
+                    ),
+                    //SizedBox(height: 10,),
+                    const Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 20, horizontal: 25),
+                      child: Text(
+                        "The first Mobile app deticated for Motion Graphics Courses",
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            bottom: const TabBar(
+              //isScrollable: true,
+              indicatorColor: Colors.white,
+              indicatorWeight: 5,
+              tabs: [
+                Tab(text: 'LOGIN'),
+                Tab(text: 'SIGN UP'),
+              ],
+            ),
+            elevation: 20,
+            titleSpacing: 40,
+          ),
+        ),
+        body: const TabBarView(children: [
+          LoginFields(),
+          Icon(Icons.safety_divider),
+        ]),
+      ),
+    );
+  }
+}
+
+class LoginFields extends StatelessWidget {
+  const LoginFields({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 30),
+      child: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: ConstColors.purple)),
+              focusedBorder: const OutlineInputBorder(
+                  borderSide:
+                      BorderSide(color: Color.fromARGB(255, 144, 51, 160))),
+              hintText: "Enter Email",
+            ),
+          ),
+        ],
       ),
     );
   }
